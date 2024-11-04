d51f6914a9b29a0ef8ba3f811772f9c6b5e94b9f
JacobD4676 <111395383+JacobD4676@users.noreply.github.com>
2024-11-04 16:12:32 -0500

    Added button component.

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -1,8 +1,38 @@
+import playLightIcon from '../icons/PlayLight.svg'
+import addLightIcon from '../icons/AddLight.svg'
+import gearLightIcon from '../icons/GearLight.svg'
+
 // Add props to change button based on desired button
 
-export default function Button() {
+enum ButtonType {
+    login, add, play, gear, signup, door, moon, done
+}
+
+interface ButtonProps {
+    buttonType: ButtonType
+    onClick: () => void // Action to take when this button is clicked
+}
+
+export default function Button(props: ButtonProps) {
+    let content;
+    switch (props.buttonType) {
+        case ButtonType.login:
+            content = <p>Login</p>
+            break
+        case ButtonType.add:
+            content = <img src={addLightIcon} alt={'Add icon'}></img>
+            break
+        case ButtonType.play:
+            content = <img src={playLightIcon} alt={'Play icon'}></img>
+            break
+        case ButtonType.gear:
+            content = <img src={gearLightIcon} alt={'Gear icon'}></img>
+            break
+    }
+
     return (
         <>
+            <button onClick={props.onClick}>{content}</button>
         </>
     )
 }
