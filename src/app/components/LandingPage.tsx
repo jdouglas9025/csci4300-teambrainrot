a4888ca0708dc7b0b416abf734bb65e3da69e3dc
Bani E <bon4328@gmail.com>
2024-11-04 16:41:25 -0500

    added basics to Landing Page

---
+++ src/app/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import Button from '/components/Button'
+
+
+export default function LandingPage() {
+    return (
+        <div>
+            <p>Logo Placeholder</p>
+            <h1>Simpli Quiz</h1>
+            <p>Simpli Create</p>
+            <p>Simpli Learn</p>
+            <Button />
+            <Button />
+        </div>
+    );
+}
