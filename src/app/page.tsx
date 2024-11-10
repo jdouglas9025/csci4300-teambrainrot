4fa0da1d9e17f4acd633e1f9155b687fe9e1f13c
Bani E <bon4328@gmail.com>
2024-11-10 18:11:24 -0500

    added simple layout to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,11 @@ import {sampleQuiz} from "@/app/interfaces";
 
 export default function Home() {
     return (
-        <LoginPage/>
-  );
+        <h1>Simpli Quiz</h1>
+        <p>Simpli Create.</p>
+        <p>Simpli Learn.</p>
+        <Button className={styles.button} type='login' buttonType={ButtonType.login} onClick={() => router.push('@/app/components/LoginPage')}/>
+        <Button className={styles.button} type='signup' buttonType={ButtonType.signup} onClick={() => router.push('@/app/components/SignupPage')}/>
+
+      );
 }
