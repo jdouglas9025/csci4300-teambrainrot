0be600e2fa0a73b9093cd4f602536785687b12d0
JacobD4676 <111395383+JacobD4676@users.noreply.github.com>
2024-11-11 17:32:09 -0500

    Added styling for landing page.

--- src/app/components/LandingPage.tsx
+++ src/app/components/LandingPage.tsx
@@ -1,24 +1,27 @@
 import Button, {ButtonType} from './Button'
-import Link from "next/link";
 import styles from "@/app/css/LandingPage.module.css";
 import React from "react";
+import Logo from "@/app/components/Logo";
+import Link from "next/link";
 
 
 export default function LandingPage() {
     return (
-        <div>
-            <p>Logo Placeholder</p>
-            <h1>Simpli Quiz</h1>
-            <p>Simpli Create</p>
-            <p>Simpli Learn</p>
+        <div className={styles.background}>
+            <div className={styles.container}>
+                <Logo className={styles.logo}></Logo>
+                <h1 className={styles.name}>Simpli Quiz</h1>
 
-            <Link href={'/login'}>
-                <Button className={styles.button} type='button' buttonType={ButtonType.login}/>
-            </Link>
+                <h2 className={styles.description}>Simpli Create</h2>
+                <h2 className={styles.description}>Simpli Learn</h2>
 
-            <Link href={'/signup'}>
-                <Button className={styles.button} type='button' buttonType={ButtonType.signup}/>
-            </Link>
+                <Link className={styles.link} href='/login'>
+                    <Button className={styles.button} type='button' buttonType={ButtonType.login}/>
+                </Link>
+                <Link className={styles.link} href='/signup'>
+                    <Button className={styles.button} type='button' buttonType={ButtonType.signup}/>
+                </Link>
+            </div>
         </div>
     );
 }
