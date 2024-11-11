cc96d358f2a5199e552f1e840f1a6dadd66586b6
Bani E <bon4328@gmail.com>
2024-11-11 17:20:19 -0500

    nav bar and settings changes

--- src/app/components/SettingsPage.tsx
+++ src/app/components/SettingsPage.tsx
@@ -8,20 +8,22 @@ import styles from "../css/SettingsPage.module.css"
 import router from "next/navigation";
 
 export default function SettingsPage() {
+    const router = useRouter();
     return (
        <>
             <NavBar
-            leftIcon={<Button buttonType={ButtonType.backArrow}/>}
-            rightIcon={<Button buttonType={ButtonType.done}/>}
+            leftIcon={<Button buttonType={ButtonType.backArrow} onClick={() => router.push('/homepage')}/>}
+            rightIcon={<Button buttonType={ButtonType.done} onClick={() => router.push('/homepage')}/>}
             title="Settings"
             editable={false}
             />
 
-            <div>
+            <div className={styles.container}>
                 <QuizPreferences />
             </div>
-            <div>
-                <Button buttonType={ButtonType.edit} onClick={() => router.push('/quizeditpage')}/>
+            <div className={styles.editSection}>
+                <Button buttonType={ButtonType.edit} onClick={() => router.push('/quizeditpage')} className={styles.editButton}/>
+                <span className={styles.deleteText}>Delete</span>
             </div>
         </>
     );
