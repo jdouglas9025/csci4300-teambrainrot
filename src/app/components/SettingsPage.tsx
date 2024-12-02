4d72c05ae8d67641339a4b930520b18ef366306e
Bani E <bon4328@gmail.com>
2024-12-02 18:09:43 -0500

    added design to settings page

--- src/app/components/SettingsPage.tsx
+++ src/app/components/SettingsPage.tsx
@@ -7,11 +7,13 @@ import QuizPreferences from "./QuizPreferences";
 import UserPreferences from "./UserPreferences"
 import styles from "../css/SettingsPage.module.css"
 import router from "next/navigation";
+import { useDarkMode } from "./DarkModeContext";
 
 export default function SettingsPage() {
     const router = useRouter();
+    const { isDarkMode, setDarkMode } = useDarkMode()
     return (
-       <>
+       <div>
             <NavBar
             leftIcon={<Button buttonType={ButtonType.backArrow} onClick={() => router.push('/homepage')}/>}
             rightIcon={<Button buttonType={ButtonType.done} onClick={() => router.push('/homepage')}/>}
@@ -22,7 +24,32 @@ export default function SettingsPage() {
             <div className={styles.userContainer}>
                 <UserPreferences />
             </div>
+
+            <hr className={styles.line1} style={{
+            filter: isDarkMode ? "invert(1)" : "invert(0)",
+            transition: "filter 0.3s ease",
+            }}/>
+
+            <hr className={styles.line2} style={{
+            filter: isDarkMode ? "invert(1)" : "invert(0)",
+            transition: "filter 0.3s ease",
+            }}/>
+
+            <hr className={styles.line3} style={{
+            filter: isDarkMode ? "invert(1)" : "invert(0)",
+            transition: "filter 0.3s ease",
+            }}/>
+
+            <hr className={styles.line4} style={{
+            filter: isDarkMode ? "invert(1)" : "invert(0)",
+            transition: "filter 0.3s ease",
+            }}/>
+
+            <hr className={styles.line5} style={{
+            filter: isDarkMode ? "invert(1)" : "invert(0)",
+            transition: "filter 0.3s ease",
+            }}/>
             
-        </>
+        </div>
     );
 }
